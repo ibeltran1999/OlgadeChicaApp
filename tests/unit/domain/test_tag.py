19767cbbba34dcd01d8b9a031e0ad80a4659861d
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-15 15:56:24 -0500

    test: se agrega test de creacion de tags (ROJO)

---
+++ tests/unit/domain/test_tag.py
@@ -0,0 +1,19 @@
+import unittest
+from faker import Faker
+from datetime import date
+from app.domain import Tag
+
+class TagTestCase(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def test_crear_tag(self):
+        tag = Tag(
+            id = "T001",
+            nombre = "Nombre del tag",
+        )
+
+        self.assertIsInstance(tag, Tag)
+        self.assertEqual(tag.id, "T001")
+        self.assertEqual(tag.nombre, "Nombre del tag")
