6b80e0b0ad04268a8ecd14fd67b0aab578f70246
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-15 15:13:17 -0500

    test: se agrega prueba de creacion de boceto (ROJO)

---
+++ tests/unit/domain/test_boceto.py
@@ -0,0 +1,19 @@
+from app.domain import Boceto
+import unittest
+from datetime import date
+
+class BocetoTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        return
+
+    def test_crear_boceto(self):
+        boceto = Boceto(
+            id="B001",
+            descripcion="Descripcion de prueba",
+            fecha=date(1977, 4, 20),
+        )
+        self.assertIsInstance(boceto, Boceto)
+        self.assertEqual(boceto.id, "B001")
+        self.assertEqual(boceto.descripcion, "Descripcion de prueba")
+        self.assertEqual(boceto.fecha, date(1977, 4, 20))
