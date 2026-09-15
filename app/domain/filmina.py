fb155acc56ab9900145bc9487d46b65ab2dca7e3
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-15 16:30:57 -0500

    test: se extiende el test de creacion de Filminas para incluir Procedencia (VERDE)

--- app/domain/filmina.py
+++ app/domain/filmina.py
@@ -1,5 +1,6 @@
 class Filmina:
-    def __init__(self, id, descripcion, fecha):
+    def __init__(self, id, descripcion, fecha, procedencia):
         self.id = id
         self.descripcion = descripcion
         self.fecha = fecha
+        self.procedencia = procedencia
