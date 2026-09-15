b868699feec2d1eed3fe3ab62eae22b9df96a63c
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-15 16:17:02 -0500

    test: se agrega test de enumeraciones de tipo de recuso (VERDE)

--- app/domain/enums.py
+++ app/domain/enums.py
@@ -3,3 +3,7 @@ from enum import Enum
 class ProcedenciaFilmina(Enum):
     ARCHIVO_PEDRO_FELIPE_HOYOS_KORVEL = "Archivo Pedro Felipe Hoyos Korvel"
     BLAA = "BLAA"
+
+class TipoRecurso(Enum):
+    OBRA_FISICA = "Obra fisica"
+    FILMINA_BIBLIOTECA = "Filmina biblioteca"
