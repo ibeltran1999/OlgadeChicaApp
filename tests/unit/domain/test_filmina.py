f38e8bfe8200560131c47523da9e92881bc4aa11
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-15 16:06:16 -0500

    test: se agrega test de enumeraciones de procedencia de Filminas (ROJO)

--- tests/unit/domain/test_filmina.py
+++ tests/unit/domain/test_filmina.py
@@ -2,6 +2,7 @@ import unittest
 from faker import Faker
 from datetime import date
 from app.domain import Filmina
+from app.domain.enums import ProcedenciaFilmina
 
 faker = Faker("es_CO")
 Faker.seed(1000)
@@ -22,4 +23,6 @@ class FilminaTestCase(unittest.TestCase):
         self.assertEqual(filmina.id, "F001")
         self.assertEqual(filmina.descripcion, "Descripcion de prueba")
         self.assertEqual(filmina.fecha, date(1977, 4, 20))
-        
+
+    def test_procedencia_filmina(self):
+        self.assertEqual(ProcedenciaFilmina.BLAA.value, "BLAA")
