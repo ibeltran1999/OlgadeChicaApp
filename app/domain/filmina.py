e67bf07a544207ee916f703a4bb1763decf01e87
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-22 12:58:45 -0500

    test: T52 Asociar hasta tres tags (VERDE)

--- app/domain/filmina.py
+++ app/domain/filmina.py
@@ -6,6 +6,7 @@ class Filmina:
         self.procedencia = procedencia
         self.bocetos = []
         self.archivo = None
+        self.tags = []
 
     def agregar_boceto(self, boceto):
         if boceto not in self.bocetos:
@@ -13,6 +14,10 @@ class Filmina:
 
         if self not in boceto.filminas:
             boceto.filminas.append(self)
+
+    def agregar_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
 
     def agregar_archivo(self, archivo):
         self.archivo = archivo
