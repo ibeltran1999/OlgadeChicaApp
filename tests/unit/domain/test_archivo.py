c2c42ac4cce0d3ef1f9808f5ee445e4a5cafe998
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-22 09:13:09 -0500

    test: actualizacion test_archivo.py para incluir data mock con Faker

--- tests/unit/domain/test_archivo.py
+++ tests/unit/domain/test_archivo.py
@@ -1,19 +1,38 @@
 import unittest
 from app.domain import Archivo
+from app.domain.enums import TipoArchivo
+from faker import Faker
 
 
 class ArchivoTestCase(unittest.TestCase):
 
     def setUp(self):
-        return
+        self.data_factory = Faker("es_CO")
+        self.data_factory.seed_instance(1000)
+
+        tipo_archivo = [tipoarchivo.value for tipoarchivo in TipoArchivo]
+
+        self.datos_archivos = []
+        for i in range(0, 10):
+            tipo = self.data_factory.random_element(tipo_archivo)
+            nombre = f"archivo_{i + 1:03d}.{tipo.lower()}"
+
+            self.datos_archivos.append(
+                {
+                    "ruta": f"archivos/{nombre}",
+                    "nombre": nombre,
+                    "tipo": TipoArchivo(tipo),
+                }
+            )
 
     def test_crear_archivo(self):
+        datos_archivo_1 = self.datos_archivos[0]
         archivo = Archivo(
-            ruta="ruta/del/archivo.pdf",
-            nombre="Nombre del archivo",
-            tipo="Tipo del archivo",
+            ruta=datos_archivo_1["ruta"],
+            nombre=datos_archivo_1["nombre"],
+            tipo=datos_archivo_1["tipo"],
         )
         self.assertIsInstance(archivo, Archivo)
-        self.assertEqual(archivo.ruta, "ruta/del/archivo.pdf")
-        self.assertEqual(archivo.nombre, "Nombre del archivo")
-        self.assertEqual(archivo.tipo, "Tipo del archivo")
+        self.assertEqual(archivo.ruta, datos_archivo_1["ruta"])
+        self.assertEqual(archivo.nombre, datos_archivo_1["nombre"])
+        self.assertEqual(archivo.tipo, datos_archivo_1["tipo"])
