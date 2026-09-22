f1b47b52d42873dd273f7de94b81bb49ccb6e251
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-22 12:15:15 -0500

    build: se corrige la referencia en clase tags

--- tests/unit/domain/test_tag.py
+++ tests/unit/domain/test_tag.py
@@ -11,10 +11,10 @@ class TagTestCase(unittest.TestCase):
 
     def test_crear_tag(self):
         tag = Tag(
-            id="T001",
+            identificador="T001",
             nombre="Nombre del tag",
         )
 
         self.assertIsInstance(tag, Tag)
-        self.assertEqual(tag.id, "T001")
+        self.assertEqual(tag.identificador, "T001")
         self.assertEqual(tag.nombre, "Nombre del tag")
