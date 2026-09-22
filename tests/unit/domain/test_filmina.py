485700520b785fc1c33239b881948ddb25fa3f16
Isaac Beltran Bolivar <i.beltran@uniandes.edu.co>
2026-09-21 21:54:31 -0500

    test: se agrega el test que verifica la relacion de una filmina con un boceto (ROJO)

--- tests/unit/domain/test_filmina.py
+++ tests/unit/domain/test_filmina.py
@@ -1,7 +1,7 @@
 import unittest
 from faker import Faker
 from datetime import date
-from app.domain import Filmina
+from app.domain import Filmina, Boceto
 from app.domain.enums import ProcedenciaFilmina
 
 faker = Faker("es_CO")
@@ -29,3 +29,24 @@ class FilminaTestCase(unittest.TestCase):
 
     def test_procedencia_filmina(self):
         self.assertEqual(ProcedenciaFilmina.BLAA.value, "BLAA")
+
+    def test_relacionar_una_filmina_con_un_boceto(self):
+        filmina_1 = Filmina(
+            id="F001",
+            descripcion="Filmina 1",
+            fecha=date(1977, 4, 20),
+            procedencia=ProcedenciaFilmina.BLAA,
+        )
+        boceto_1 = Boceto(
+            id="B001",
+            descripcion="Boceto 1",
+            fecha=date(1977, 4, 20),
+        )
+        filmina_1.agregar_boceto(boceto_1)
+
+        self.assertEqual(len(filmina_1.bocetos), 1)
+        self.assertEqual(len(boceto_1.filminas), 1)
+        self.assertIn(boceto_1, filmina_1.bocetos)
+        self.assertIn(filmina_1, boceto_1.filminas)
+        self.assertIs(filmina_1.bocetos[0], boceto_1)
+        self.assertIs(boceto_1.filminas[0], filmina_1)
